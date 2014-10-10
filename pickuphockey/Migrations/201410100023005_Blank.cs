4ba9ece324602ec50c24825cf57e0ee7b95a1bf4
Brett Morrison <b@brettmorrison.com>
2014-10-09 17:25:57 -0700

    Adding PayPal button and Sent / Received WIP

---
+++ pickuphockey/Migrations/201410100023005_Blank.cs
@@ -0,0 +1,16 @@
+namespace pickuphockey.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Blank : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- pickuphockey/Scripts/main.js
+++ pickuphockey/Scripts/main.js
@@ -1,3 +1,13 @@
 ﻿$(document).ready(function () {
     $('#SessionDate').datepicker({ dateFormat: "DD, mm/dd/yy" });
 });
+
+$(document).ready(function () {
+    $("[id^=PaymentSent_]").change(function () {
+        var checked = $(this).is(':checked');
+    });
+
+    $("[id^=PaymentReceived_]").change(function () {
+        var checked = $(this).is(':checked');
+    });
+});
