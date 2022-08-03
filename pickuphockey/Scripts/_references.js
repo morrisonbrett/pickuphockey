725d82946f60e712d4d99db53038137e788e4945
Brett Morrison <b@brettmorrison.com>
2022-08-03 12:02:30 -0700

    Updated Packages

--- pickuphockey/Scripts/_references.js
+++ pickuphockey/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-ui-1.13.1.js" />
+﻿/// <reference path="jquery-ui-1.13.2.js" />
 /// <reference path="jquery-3.6.0.js" />
 /// <autosync enabled="true" />
 /// <reference path="ai.0.22.9-build00167.min.js" />
