9a8b62e7122c7c9c77df0aece6ec96c6c28e6e90
Brett Morrison <b@brettmorrison.com>
2022-10-31 13:36:29 -0700

    Fixed where clause for cancelled sessions

--- scripts/Reports.sql
+++ scripts/Reports.sql
@@ -1,18 +1,18 @@
 /* Number of sessions by day of week */
 select datename(weekday, sessiondate) as Weekday, count(sessionid) as '# of Sessions' from Sessions
-where not (note is not null and note like '%cancelled%')
+where note not like '%cancelled%'
 group by datename(weekday, sessiondate)
 order by count(sessionid) desc
 
 /* Number of sessions by month */
 select datename(month, sessiondate) as Month, count(sessionid) as '# of Sessions' from Sessions
-where not (note is not null and note like '%cancelled%')
+where note not like '%cancelled%'
 group by datename(month, sessiondate), month(sessiondate)
 order by count(sessionid) desc
 
 /* Number of sessions by year */
 select year(sessiondate) as Year, count(sessionid) as '# of Sessions' from Sessions
-where not (note is not null and note like '%cancelled%')
+where note not like '%cancelled%'
 group by year(sessiondate)
 order by year(sessiondate)
 
@@ -29,6 +29,6 @@ GROUP BY BuyerUserId, FirstName, LastName
 ORDER BY COUNT(BuyerUserId) DESC
 
 /* Recent Activities, shows SessionDate column for context */
-SELECT TOP 100 ActivityLogs.*, Sessions.SessionDate from ActivityLogs
+SELECT TOP 200 ActivityLogs.*, Sessions.SessionDate from ActivityLogs
 INNER JOIN Sessions ON ActivityLogs.SessionId = sessions.SessionId
 ORDER BY ActivityLogs.CreateDateTime DESC
