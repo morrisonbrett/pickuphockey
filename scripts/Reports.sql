614303e6dfcd89010330bc301a512c759a548995
Brett Morrison <b@brettmorrison.com>
2023-10-18 15:25:39 -0700

    Added variables for Reports for Sellers by Year

--- scripts/Reports.sql
+++ scripts/Reports.sql
@@ -42,21 +42,26 @@ WHERE BuyerUserId != SellerUserId
 GROUP BY SellerUserId, FirstName, LastName
 ORDER BY COUNT(SellerUserId) DESC
 
-/* Top Sellers in 2022 */
+/* Top Sellers - Adjust year and day variables */
+DECLARE @SellerYear INT
+SET @SellerYear = 2023
+DECLARE @SellerWeekday nvarchar(10)
+SET @SellerWeekday = 'Friday'
+
 SELECT T1.FirstName, T1.LastName, T1.SellerCount, T2.SessionCount, CAST((CAST(T1.SellerCount AS decimal) / CAST(T2.SessionCount AS decimal)) AS decimal(18,4)) * 100 AS SellingPercentage, T2.Year, T2.Weekday FROM
 (SELECT SellerUserId, FirstName, LastName, COUNT(SellerUserId) AS SellerCount from BuySells
 INNER JOIN AspNetUsers on SellerUserId = AspNetUsers.id
 INNER JOIN Sessions on BuySells.SessionId = Sessions.SessionId
 LEFT OUTER JOIN SessionsByDate on YEAR(Sessions.SessionDate) = YEAR(SessionsByDate.Year)
-WHERE YEAR(Sessions.SessionDate) = 2019 AND DATENAME(WeekDay, Sessions.SessionDate) = 'Friday'
+WHERE YEAR(Sessions.SessionDate) = @SellerYear AND DATENAME(WeekDay, Sessions.SessionDate) = @SellerWeekday
 AND BuyerUserId != SellerUserId
 GROUP BY SellerUserId, FirstName, LastName
 ) AS T1,
-(SELECT * from SessionsByDate WHERE YEAR = 2019 AND SessionsByDate.Weekday = 'Friday') AS T2
+(SELECT * from SessionsByDate WHERE YEAR = @SellerYear AND SessionsByDate.Weekday = @SellerWeekday) AS T2
 ORDER BY T1.SellerCount DESC
 
 /* Top Buyers */
-SELECT BuyerUserId, FirstName, LastName, COUNT(BuyerUserId) AS BuyerCount, MAX(SessionDate) As LastSessionBought from BuySells
+SELECT FirstName, LastName, COUNT(BuyerUserId) AS BuyerCount, MAX(SessionDate) As LastSessionBought from BuySells
 INNER JOIN AspNetUsers on BuyerUserId = AspNetUsers.id
 INNER JOIN Sessions on Sessions.SessionId = BuySells.SessionId
 WHERE SellerUserId != BuyerUserId
